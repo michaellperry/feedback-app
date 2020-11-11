9007199efe88ea906dc8e72c5562e6e66d71d153
Michael L Perry <michael@qedcode.com>
2020-11-10 23:05:04 -0600

    Simple updates

--- src/server/images.ts
+++ src/server/images.ts
@@ -30,9 +30,9 @@ function safeFormat(format: string) {
 
 export async function renderImage(req: Request, res: Response, imageFileName: string) {
   // Extract the query-parameter
-  const widthString = req.query.width;
-  const heightString = req.query.height;
-  const format = req.query.format;
+  const widthString = req.query.width.toString();
+  const heightString = req.query.height.toString();
+  const format = req.query.format.toString();
 
   // Parse to integer if possible
   let width, height;
